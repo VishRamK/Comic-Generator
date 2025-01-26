42027d95db05174ea31a71b013c9f3f4cfd18bc4
Vishruth Konakanchi <vish2004@mit.edu>
2025-01-26 02:00:18 -0500

    Repaired

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "replicate.delivery",
       },
+      {
+        protocol: "https",
+        hostname: "pbxt.replicate.delivery",
+      }
     ],
   }
 };
